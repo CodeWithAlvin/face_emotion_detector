743ef3e468afa9cd535e9fa98dd79955adf1630e
Alvin Saini <70733646+CodeWithAlvin@users.noreply.github.com>
2021-02-02 11:06:25 +0530

    Add files via upload

---
+++ live.py
@@ -0,0 +1,16 @@
+import pickle
+import cv2
+
+emotion_dict = {0: "Angry", 1: "Disgust", 2: "Fear", 3: "Happy", 4: "Sad", 5: "Surprise", 6: "Neutral"}
+
+with open("model.pkl","rb") as f:
+    model=pickle.load(f)
+
+video=cv2.VideoCapture(0)
+
+while True:
+    _,f=video.read()
+    res=model.predict([cv2.cvtColor(cv2.resize(f,(48,48)),cv2.COLOR_BGR2GRAY).flatten()])
+    cv2.putText(f,emotion_dict[res[0]],(25, 20),cv2.FONT_HERSHEY_SIMPLEX,1,(255, 0, 0),2)
+    cv2.imshow("Emotion Detection",f)
+    cv2.waitKey(1)
