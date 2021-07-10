6e4d00cc745ef6411e6b12be88b7c9b038dfe3a4
alvin saini <codewithalvin@gmail.com>
2021-07-10 08:52:36 +0530

    changes made

--- live.py
+++ live.py
@@ -1,29 +1,23 @@
-import pickle
 import cv2
 import numpy as np
+from tensorflow import keras
 
 emotion_dict = {0: "Angry", 1: "Disgust", 2: "Fear", 3: "Happy", 4: "Sad", 5: "Surprise", 6: "Neutral"}
 
-model_path="/models/conv3-5-july.h5"
+model_path="models\CNN(64,128,512,512)Dense(512,1024)model.h5"
 
-def load_model(model_path,type="keras"):
-    #type --> pickle | keras
-    if type == "pickle":
-        with open(model_path,"rb") as f:
-            return pickle.load(f)
-    elif type == "keras":
-        from tensorflow import keras
-        return keras.model.load(model_path)
-    else:
-        raise ValueError("unknown value {type} must be either pickle or keras")
+def load_model(model_path):
+    return keras.models.load_model(model_path)
 
-model=load_model(model_path,"keras")
+model=load_model(model_path)
 
 video=cv2.VideoCapture(0)
 
 while True:
     _,f=video.read()
-    res=model.predict([cv2.cvtColor(cv2.resize(f,(48,48)),cv2.COLOR_BGR2GRAY).flatten()])
-    cv2.putText(f,emotion_dict[np.argmax(result)],(25, 20),cv2.FONT_HERSHEY_SIMPLEX,1,(255, 0, 0),2)
+    transform=cv2.cvtColor(cv2.resize(f,(48,48)),cv2.COLOR_BGR2GRAY).reshape(-1,48,48,1)
+    res=model.predict([transform])
+    cv2.putText(f,emotion_dict[np.argmax(res)],(25, 20),cv2.FONT_HERSHEY_SIMPLEX,1,(255, 0, 0),2)
     cv2.imshow("Emotion Detection",f)
     cv2.waitKey(1)
+    
