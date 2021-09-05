34fc66837cacd874a9645123c8b2ad08331742af
Alvin Saini <70733646+CodeWithAlvin@users.noreply.github.com>
2021-09-05 22:43:14 +0530

    Update live.py

--- live.py
+++ live.py
@@ -1,26 +1,53 @@
+# Importing all required packages
 import cv2
 import numpy as np
 from tensorflow import keras
 
+print("import done")
+
+# Read in the cascade classifiers for face and eyes
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# create a function to detect face
+def adjusted_detect_face(img):
+    face_rect = face_cascade.detectMultiScale(img,
+                                              scaleFactor = 1.2,
+                                              minNeighbors = 5)
+    padding=20
+    for (x, y, w, h) in face_rect:
+        img = img[x:x + (w+(3*padding)),  y:y + (h+(3*padding))]
+	    
+    return img
+
 emotion_dict = {0: "Angry", 1: "Disgust", 2: "Fear", 3: "Happy", 4: "Sad", 5: "Surprise", 6: "Neutral"}
 
-model_path="models\CNN(64,128,512,512)Dense(512,1024)model.h5"
+print("loading model ")
+model=keras.models.load_model("4CNN,4LSTM,2DENSE model.h5")
+print("model loaded")
 
-def load_model(model_path):
-    return keras.models.load_model(model_path)
+def predict(img):
+    img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    img=cv2.resize(img,(48,48))
+    img=img/255
+    img=img.reshape(-1,48,48,1)
+    res=model.predict(img)
+    res=np.argmax(res)
+    return emotion_dict[res]
 
-model=load_model(model_path)
 
-video=cv2.VideoCapture(0)
-
-while True:
-    _,f=video.read()
-    transform=cv2.cvtColor(cv2.resize(f,(48,48)),cv2.COLOR_BGR2GRAY)/255
-    res=model.predict([transform.reshape(-1,48,48,1)])
-    cv2.putText(f,emotion_dict[np.argmax(res)],(25, 20),cv2.FONT_HERSHEY_SIMPLEX,1,(255, 0, 0),2)
-    cv2.imshow("Emotion Detection",f)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    
-video.release()
-cv2.destroyAllWindows()
+if __name__=="__main__":
+    cap = cv2.VideoCapture(0)
+    while cap.isOpened():
+        success, image = cap.read()
+        if not success:
+            print("Ignoring empty camera frame.")
+            # If loading a video, use 'break' instead of 'continue'.
+            continue
+        img=adjusted_detect_face(image)
+        res=predict(img)
+        cv2.putText(image, res, (20, 70), cv2.FONT_HERSHEY_PLAIN, 1, (0, 196, 255), 2)
+        cv2.imshow('face Detection', image)
+        cv2.imshow('mdl img', img)
+        if cv2.waitKey(5) & 0xFF == ord('q'):
+            break
+    cv2.destroyAllWindows()
